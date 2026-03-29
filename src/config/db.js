72d0892ed04be7821c2425953780c270779f67cd
Sridhar J <sridharjeyakumar@gmail.com>
2026-03-29 23:45:50 +0530

    remove db failuer issue

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Error: ${error.message}`);
-    process.exit(1);
   }
 };
 
